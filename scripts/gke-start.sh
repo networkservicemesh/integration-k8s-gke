819346e800a38ce089cf777fd8e95511de50030b
Mikhail Avramenko <avramenkomihail15@gmail.com>
2021-12-22 16:06:39 +0700

    f
    Signed-off-by: Mikhail Avramenko <avramenkomihail15@gmail.com>

--- scripts/gke-start.sh
+++ scripts/gke-start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 if ! gcloud container clusters list --project="${GKE_PROJECT_ID}" | grep -q ^"${GKE_CLUSTER_NAME}"; then \
-  time gcloud container clusters create "${GKE_CLUSTER_NAME}" --project="${GKE_PROJECT_ID}" --machine-type="${GKE_CLUSTER_TYPE}" --num-nodes="${GKE_CLUSTER_NUM_NODES}" --zone="${GKE_CLUSTER_ZONE}" -q; \
+  time gcloud container clusters create "${GKE_CLUSTER_NAME}" --project="${GKE_PROJECT_ID}" --machine-type="${GKE_CLUSTER_TYPE}" --num-nodes="${GKE_CLUSTER_NUM_NODES}" --zone="${GKE_CLUSTER_ZONE}" --node-version=1.22.3-gke.1500 -q; \
   echo "Writing config to ${KUBECONFIG}"; \
   gcloud container clusters get-credentials "${GKE_CLUSTER_NAME}" --project="${GKE_PROJECT_ID}" --zone="${GKE_CLUSTER_ZONE}" ; \
   gcloud compute firewall-rules create allow-proxy-nsm --action ALLOW --rules tcp:80 --project="${GKE_PROJECT_ID}"; \
