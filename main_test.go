0da4a6df6240fd852af429e2065a49835d1f34dd
Nikita Skrynnik <nikita.skrynnik@xored.com>
2022-01-31 21:36:53 +1100

    delete other tests
    Signed-off-by: Nikita Skrynnik <nikita.skrynnik@xored.com>

--- main_test.go
+++ main_test.go
@@ -22,28 +22,8 @@ import (
 	"github.com/stretchr/testify/suite"
 	_ "k8s.io/client-go/plugin/pkg/client/auth/gcp" // This is required for GKE authentication
 
-	"github.com/networkservicemesh/integration-tests/suites/basic"
-	"github.com/networkservicemesh/integration-tests/suites/features"
-	"github.com/networkservicemesh/integration-tests/suites/heal"
-	"github.com/networkservicemesh/integration-tests/suites/memory"
 	"github.com/networkservicemesh/integration-tests/suites/observability"
 )
-
-func TestRunFeatureSuite(t *testing.T) {
-	suite.Run(t, new(features.Suite))
-}
-
-func TestRunBasicSuite(t *testing.T) {
-	suite.Run(t, new(basic.Suite))
-}
-
-func TestRunMemorySuite(t *testing.T) {
-	suite.Run(t, new(memory.Suite))
-}
-
-func TestRunHealSuite(t *testing.T) {
-	suite.Run(t, new(heal.Suite))
-}
 
 func TestRunObservabilitySuite(t *testing.T) {
 	suite.Run(t, new(observability.Suite))
