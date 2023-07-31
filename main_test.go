57921085e617501c8ed84aff92a12ffb97f13662
Nikita Skrynnik <nikita.skrynnik@xored.com>
2023-07-31 18:12:13 +0700

    run the whole memory suite
    Signed-off-by: Nikita Skrynnik <nikita.skrynnik@xored.com>

--- main_test.go
+++ main_test.go
@@ -19,72 +19,12 @@
 package main_test
 
 import (
-	"fmt"
-	"os"
-	"os/exec"
-	"path/filepath"
 	"testing"
 
 	"github.com/networkservicemesh/integration-tests/suites/memory"
 	"github.com/stretchr/testify/suite"
 )
 
-func TestExample(t *testing.T) {
-	artsDir := os.Getenv("ARTIFACTS_DIR")
-	if artsDir == "" {
-		artsDir = "logs"
-	}
-
-	mem := new(memory.Suite)
-	mem.SetT(t)
-	r := mem.Runner(".")
-
-	cmd := exec.Command("pwd")
-	stdout, _ := cmd.Output()
-	fmt.Printf("pwd: %s\n", string(stdout))
-
-	cmd = exec.Command("ls")
-	stdout, _ = cmd.Output()
-	fmt.Printf("ls: %s\n", string(stdout))
-
-	stdout, err := exec.Command("kubectl", "config", "view").Output()
-	fmt.Printf("kubectl config view: %s err: %v\n", string(stdout), err)
-
-	r.Run("kubectl config view")
-
-	var singleClusterKubeConfig = os.Getenv("KUBECONFIG")
-	if singleClusterKubeConfig == "" {
-		singleClusterKubeConfig = filepath.Join(os.Getenv("HOME"), ".kube", "config")
-	}
-
-	r.Run(fmt.Sprintf("cat %s", singleClusterKubeConfig))
-	fmt.Printf("singleClusterKubeconfig: %s\n", singleClusterKubeConfig)
-
-	r.Run(fmt.Sprintf("kubectl cluster-info --kubeconfig \"%s\" dump --output-directory=logs --all-namespaces --v=9", singleClusterKubeConfig))
-}
-
-type calicoFeatureSuite struct {
-	memory.Suite
-}
-
-func (s *calicoFeatureSuite) BeforeTest(suiteName, testName string) {
-	switch testName {
-	case
-		"TestKernel2kernel",
-		"TestKernel2ethernet2kernel":
-		s.T().Skip()
-	}
-}
-
 func TestRunMemorySuite(t *testing.T) {
-	cmd := exec.Command("pwd")
-	stdout, _ := cmd.Output()
-	fmt.Printf("pwd: %s\n", string(stdout))
-
-	//os.Setenv("KUBECONFIG", "/home/nikita/.kube/config")
-	suite.Run(t, new(calicoFeatureSuite))
-
-	cmd = exec.Command("ls")
-	stdout, _ = cmd.Output()
-	fmt.Printf("ls: %s\n", string(stdout))
+	suite.Run(t, new(memory.Suite))
 }
